8b2464ad823968bf6b626255571198fbb2ef808c
Nikolay <nikolaymax@inbox.ru>
2018-11-25 20:43:29 +0300

    ивыта

--- eclipse-workspace/nastya/src/nastya/SimpleGUI.java
+++ eclipse-workspace/nastya/src/nastya/SimpleGUI.java
@@ -12,16 +12,6 @@ public class SimpleGUI extends JFrame {
 
 	/**
 	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
 	 */
 	
 	private static final long serialVersionUID = 1L;
@@ -31,13 +21,8 @@ public class SimpleGUI extends JFrame {
 	public static JTextField textField3;
 	public static String str = "Ответ:  ";
 	public static boolean k = true;
-	
-	public static boolean ty = true;
-	public static boolean tg = true;
-	public static boolean tr = true;
-	
-	public static void main(String[] args) {
 		
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -62,7 +47,7 @@ public SimpleGUI() {
 		setContentPane(contentPane);
 		
 		textField = new JTextField();
-		textField.setBorder(BorderFactory.createLineBorder (new Color (0, 0, 0, 0), 2));
+		textField.setBorder(BorderFactory.createLineBorder (new Color (0, 0, 1, 0), 2));
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		textField.setHorizontalAlignment(SwingConstants.CENTER);
 		textField.setBackground(Color.WHITE);
@@ -71,7 +56,7 @@ public SimpleGUI() {
 		
 		textField2 = new JTextField(5);
 		textField2.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		textField2.setBorder(BorderFactory.createLineBorder (new Color (0, 0, 0, 0), 2));
+		textField2.setBorder(BorderFactory.createLineBorder (new Color (0, 23, 0, 0), 2));
 		textField2.setHorizontalAlignment(SwingConstants.LEFT);
 		textField2.setBackground(Color.WHITE);
 		textField2.setEditable(false);
@@ -109,7 +94,7 @@ public SimpleGUI() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				button5.setVisible(false);
-				textField2.setBorder(BorderFactory.createLineBorder (new Color (0, 0, 0, 0), 2));
+				textField2.setBorder(BorderFactory.createLineBorder (new Color (0, 56, 0, 0), 2));
 				textField2.setText("");
 				str = "Ответ:  ";
 				str = str + Integer.toString(prost.getRandomPrime());
